4f4e358632d978a8886e0f593ae7ca5547a96cdf
daniel antohe <dantohe@gmail.com>
2016-03-01 14:19:40 -0700

    regression changes

--- regression_play.R
+++ regression_play.R
@@ -1,50 +1,12 @@
-#Write a function called best that take two arguments: the 2-character abbreviated name of a state and an
-#outcome name. The function reads the outcome-of-care-measures.csv le and returns a character vector
-#with the name of the hospital that has the best (i.e. lowest) 30-day mortality for the specied outcome
-#in that state. The hospital name is the name provided in the Hospital.Name variable. The outcomes can
-#be one of \heart attack", \heart failure", or \pneumonia". Hospitals that do not have data on a particular
-#outcome should be excluded from the set of hospitals when deciding the rankings.
+library(dplyr)
+freqData <- as.data.frame(table(galton$child, galton$parent))
+names(freqData) <- c("child", "parent", "freq")
+freqData$child <- as.numeric(as.character(freqData$child))
+freqData$parent <- as.numeric(as.character(freqData$parent))
+g <- ggplot(filter(freqData, freq > 0), aes(x = parent, y = child))
+g <- g  + scale_size(range = c(2, 20), guide = "none" )
+g <- g + geom_point(colour="grey50", aes(size = freq+20, show_guide = FALSE))
+g <- g + geom_point(aes(colour=freq, size = freq))
+g <- g + scale_colour_gradient(low = "lightblue", high="white")                    
+g
 
-best <- function(state, outcome) {
-        ## Read outcome data
-        outcomeData<-loadDataFile()
-        
-        ## Check that state and outcome are valid
-        statesInData<-levels(factor(outcomeData$State))
-        if(!is.element(state,statesInData)){
-                stop("invalid state")
-        }
-        validOutcomes<-c("heart attack","heart failure","pneumonia")
-        if(!is.element(outcome,validOutcomes)){
-                stop("invalid outcome")
-        }
-        
-        #pick only the required state data
-        d2<-outcomeData[outcomeData$State==state,]
-        
-        #convert to numeric and keep only what's required
-        #d3<-data.frame(hospitalName=d2$Hospital.Name, ha=as.numeric(d2$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack), hf=as.numeric(d2$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure), pn=as.numeric(d2$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia) )
-        
-        d3<-data.frame(hospitalName=d2$Hospital.Name, ha=suppressWarnings(as.numeric(d2$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack)), hf=suppressWarnings(as.numeric(d2$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)), pn=suppressWarnings(as.numeric(d2$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)) )
-        
-        #order by the desired requirement
-        
-        
-        if(outcome=="heart attack"){
-                d41<-d3[with (d3, order(ha,hospitalName)),]
-        }
-        else if(outcome=="heart failure"){
-                d41<-d3[with (d3, order(hf,hospitalName)),]
-        }
-        else if(outcome=="pneumonia"){
-                d41<-d3[with (d3, order(pn,hospitalName)),]
-        }
-        result<-as.character(d41$hospitalName[1])
-        return(result)
-        
-}
-
-loadDataFile<-function(){
-        outcome<-read.csv("R:/data/GD/OurDocuments/family/Daniel/US_education/Coursera/R Programming/programming assignment 3/outcome-of-care-measures.csv", colClasses = "character")
-        return(outcome)
-}
