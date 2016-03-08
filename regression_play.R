a8ca20566c2af3559c330742754ed443bda9dec8
daniel antohe <dantohe@gmail.com>
2016-03-08 14:33:07 -0700

    more galston father son plotting

--- regression_play.R
+++ regression_play.R
@@ -44,3 +44,38 @@ manipulate(myPlot(beta), beta = slider(0.6, 1.2, step = 0.02))
 
 #linear model
 lm(I(child - mean(child))~ I(parent - mean(parent)) - 1, data = galton)
+
+y <- galton$child
+x <- galton$parent
+beta1 <- cor(y, x) * sd(y) / sd(x)
+beta0 <- mean(y) - beta1 * mean(x)
+rbind(c(beta0, beta1), coef(lm(y ~ x)))
+
+lm(x~y)
+coef(lm(x~y))
+
+x <- rnorm(100)
+y <- rnorm(100)
+odr <- order(x)
+x[odr[100]]
+x[odr[1]]
+x
+
+#
+library(UsingR)
+data(father.son)
+y <- (father.son$sheight - mean(father.son$sheight)) / sd(father.son$sheight)
+x <- (father.son$fheight - mean(father.son$fheight)) / sd(father.son$fheight)
+rho <- cor(x, y)
+library(ggplot2)
+g = ggplot(data.frame(x, y), aes(x = x, y = y))
+g = g + geom_point(size = 5, alpha = .2, colour = "black")
+g = g + geom_point(size = 4, alpha = .2, colour = "red")
+g = g + geom_vline(xintercept = 0)
+g = g + geom_hline(yintercept = 0)
+g = g + geom_abline(position = "identity")
+g = g + geom_abline(intercept = 0, slope = rho, size = 2)
+g = g + geom_abline(intercept = 0, slope = 1 / rho, size = 2)
+g = g + xlab("Father's height, normalized")
+g = g + ylab("Son's height, normalized")
+g
