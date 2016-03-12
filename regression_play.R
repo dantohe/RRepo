cd0e7fc586d3c34b98c49372e04548c69de69ce4
daniel antohe <dantohe@gmail.com>
2016-03-12 07:34:50 -0700

    more regression examples

--- regression_play.R
+++ regression_play.R
@@ -116,5 +116,63 @@ coef(fit)[1] + coef(fit)[2] * newx
 #with a function
 predict(fit, newdata = data.frame(carat=newx))
 
+plot(diamond)
+
+x <- diamond$price
+y <- diamond$carat
+
+fit <- lm(x~y)
+plot(fit)
+
+#getting residuals
+e <- resid(fit)
+e
+yhat <- predict(fit)
+yhat
+#sum of the residuals should be 0
+sum(e)
+
+#plotting in a diffrent way 
+plot(diamond$carat, diamond$price, xlab = "Mass - carats -0.2g", ylab = "Price - Singapore $", bg="lightblue", col="black", cex=1.1, pch=21, frame=FALSE)
+abline(fit, lwd=2)
+for(i in 1:n)
+        lines(c(x[i],x[i]), c(y[1],yhat[i]), col="red", lwd=2)
+
+
+#more residual examples - non linear
+x = runif(100,-3,3); y=x+sin(x) +rnorm(100, sd=.2);
+library(ggplot2)
+g = ggplot(data.frame(x = x,y = y), aes( x=x, y=y))
+g = g + geom_smooth( method ="lm", colour= "black")
+g = g + geom_point(size=7, colour ="black", alpha=0.4)
+g = g + geom_point(size=5, colour ="red", alpha=0.4)
+g
+
+#plot the residuals
+g = ggplot(data.frame(x = x,y = resid(lm(x~y))), aes( x=x, y=y))
+#g = g + geom_smooth( method ="lm", colour= "black")
+g = g + geom_hline(yintercept=0, Size=2)
+g = g + geom_point(size=7, colour ="black", alpha=0.4)
+g = g + geom_point(size=5, colour ="red", alpha=0.4)
+g = g + xlab("X") + ylab("Residual")
+g
+
+#Finding residual variance estimates
+y <- diamond$price; x <- diamond$carat; n <- length(y)
+fit <- lm(y ~ x)
+summary(fit)$sigma
+sqrt(sum(resid(fit)^2) / (n - 2))
+
+#r-squared  - the percentage of the total variability that is explained by the model
+
+
+
+
+
+
+
+
+             )
+
 
 
