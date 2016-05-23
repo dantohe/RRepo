03fd978996121f98773b7df8ad7e044a3aeffde5
daniel antohe <dantohe@gmail.com>
2016-05-23 05:49:36 -0600

    finishing galton analysis

--- galton.R
+++ galton.R
@@ -108,5 +108,41 @@ cor(x,x)
 #galton data
 #child.height = intercept + parent.height*slope
 #linear least square LLS
+cor(galton$child, galton$parent)
+cor(galton$parent, galton$child)
+l = lm(galton$child~galton$parent)
+l1 = lm(galton$parent~galton$child)
+summary(l)
+summary(l1)
+cor(galton$child, galton$parent)
+simple.lm(galton$child, galton$parent)
 
+
+data("father.son")
+help("father.son")
+plot(sheight ~ fheight, data=father.son,bty="l",pch=20)
+abline(a=0,b=1,lty=2,lwd=2)
+abline(lm(sheight ~ fheight, data=father.son),lty=1,lwd=2)
+
+#normalizing
+y = (father.son$sheight - mean(father.son$sheight))/sd(father.son$sheight)
+x = (father.son$fheight - mean(father.son$fheight))/sd(father.son$fheight)
+mean(x)
+mean(y)
+sd(x)
+sd(y)
+
+sho = cor(x,y)
+sho
+g = ggplot(data.frame(x = x, y = y), aes(x = x, y = y))
+g = g + geom_point(size = 6, colour = "black", alpha = 0.2)
+g = g + geom_point(size = 4, colour = "salmon", alpha = 0.2)
+g = g + xlim(-4, 4) + ylim(-4, 4)
+g = g + geom_abline(intercept = 0, slope = 1)
+g = g + geom_vline(xintercept = 0)
+g = g + geom_hline(yintercept = 0)
+rho <- cor(x, y)
+g = g + geom_abline(intercept = 0, slope = rho, size = 2)
+g = g + geom_abline(intercept = 0, slope = 1 / rho, size = 2)
+g
 
