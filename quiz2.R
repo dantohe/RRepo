a8ab1227d7ff91b9668fe76c84dbaa37d2365cf7
daniel antohe <dantohe@gmail.com>
2016-05-24 14:52:20 -0600

    quiz 2

---
+++ quiz2.R
@@ -0,0 +1,18 @@
+
+x <- c(0.61, 0.93, 0.83, 0.35, 0.54, 0.16, 0.91, 0.62, 0.62)
+y <- c(0.67, 0.84, 0.6, 0.18, 0.85, 0.47, 1.1, 0.65, 0.36)
+
+summary(lm(y~x))
+
+data("mtcars")
+plot(mtcars$wt, mtcars$mpg)
+abline(lm(mpg~wt, data = mtcars))
+help("mtcars")
+summary(lm(mpg~wt, data = mtcars))
+
+model = lm(mpg~wt, data = mtcars)
+new.data = c(3)
+newdata = data.frame(wt=new.data)
+predict(model, newdata, interval = "prediction")
+        
+        
