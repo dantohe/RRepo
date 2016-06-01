8501ef3fa0715bfb02d25ff64e34ee30ba97557b
daniel antohe <dantohe@gmail.com>
2016-06-01 06:21:57 -0600

    regression models using mtcars dataset 2

--- tempR.R
+++ tempR.R
@@ -7,3 +7,19 @@ head(mtcars)
 library(UsingR)
 simple.eda(mtcars$mpg)
 ?simple.eda
+
+require(ggplot2)
+require(GGally)
+
+names(mtcars)
+mtcars.reduced = mtcars[c("mpg", "cyl","am","gear")]
+
+mtcars.reduced.2 = mtcars[c("am","cyl","gear", "mpg")]
+
+g = ggpairs(mtcars.reduced.2, lower = list(
+        continuous = "smooth",
+        combo = "facetdensity")
+)
+g= ggpairs(mtcars, mapping = aes(color = am), columns =c("am","cyl","gear", "mpg"),continuous = "smooth" )
+g
+pairs(mtcars.reduced)
