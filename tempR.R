84fc8c6f495899792cce3aad15874aa5a06a8013
daniel antohe <dantohe@gmail.com>
2016-06-02 05:29:13 -0600

    analyze correlations

--- tempR.R
+++ tempR.R
@@ -20,6 +20,45 @@ g = ggpairs(mtcars.reduced.2, lower = list(
         continuous = "smooth",
         combo = "facetdensity")
 )
-g= ggpairs(mtcars, mapping = aes(color = am), columns =c("am","cyl","gear", "mpg"),continuous = "smooth" )
+
+
+
+
+g = ggpairs(mtcars, columns =c("am","cyl","gear", "mpg"), lower = list(
+        continuous = "smooth",
+        combo = "facetdensity", diag = NULL)
+)
 g
+
+
+correlations.plot = ggpairs(mtcars, columns =c("mpg","cyl","gear", "am"),  
+                    columnLabels = c("MPG", "Numbers of Cylinders", "Number of Gears", "Transmission"), lower = list(
+                    continuous = "smooth",
+                    combo = "facetdensity", diag = NULL)
+)
+correlations.plot
+
+
+correlations.plot = ggpairs(mtcars, columns =c("mpg","cyl","am"),  
+                            columnLabels = c("MPG", "Numbers of Cylinders","Transmission"), lower = list(
+                                    continuous = "smooth",
+                                    combo = "facetdensity", diag = NULL)
+)
+correlations.plot
+
+g = ggpairs(mtcars, columns =c("am","cyl","gear", "mpg"))
+
+g
+
 pairs(mtcars.reduced)
+
+
+
+
+g = ggplot(mtcars, aes(x = cyl, y = mpg))
+g = g + xlab("NUmber of Cylinders")
+g = g + ylab("MPG")
+g = g + geom_point(size = 7, colour = "black", alpha=0.5)
+g = g + geom_point(size = 5, colour = "blue", alpha=0.2)
+g = g + geom_smooth(method = "lm", colour = "black")
+g
