357b69985a1ca1bf4f7c875d593f0b1c5192e12e
daniel antohe <dantohe@gmail.com>
2016-06-03 14:06:35 -0600

    starting the multivariable regression

--- tempR.R
+++ tempR.R
@@ -122,7 +122,75 @@ for (i in 1 : n)
         lines(c(x[i], x[i]), c(e[i], 0), col = "red" , lwd = 2)
 
 
-plot(mtcars$wt, resid(lm(mtcars$mpg, mtcars$wt)), xlab = "Car Weight", ylab = "Residuals MPG", bg = "lightblue", col = "black", cex = 2, pch = 21,frame = FALSE)
+plot(mtcars$wt, resid(lm(mtcars$mpg~mtcars$wt)), xlab = "Car Weight", ylab = "Residuals MPG", bg = "lightblue", col = "black", cex = 2, pch = 21,frame = FALSE)
 abline(h = 0, lwd = 2)
 for (i in 1 : length(mtcars$wt)) 
-        lines(c(x[i], x[i]), c(e[i], 0), col = "red" , lwd = 2)
+        lines(c(mtcars$wt[i], mtcars$wt[i]), c(resid(fit)[i], 0), col = "red" , lwd = 2)
+
+
+e = c(resid(lm(price ~ 1, data = diamond)),
+      resid(lm(price ~ carat, data = diamond)))
+fit = factor(c(rep("Itc", nrow(diamond)),
+               rep("Itc, slope", nrow(diamond))))
+g = ggplot(data.frame(e = e, fit = fit), aes(y = e, x = fit, fill = fit))
+g = g + geom_dotplot(binaxis = "y", size = 2, stackdir = "center", binwidth = 20)
+g = g + xlab("Fitting approach")
+g = g + ylab("Residual price")
+g
+
+
+model.1 = lm(mpg~cyl, data = mtcars)
+model.2 = lm(mpg~wt, data = mtcars)
+model.3 = lm(mpg~am, data = mtcars)
+anova(model.1, model.2, model.3)
+
+
+
+summary(lm(mpg ~ . , data = mtcars))$coef
+
+
+initial.model <- lm(mpg ~., data= mtcars)
+best.model <- step(initial.model, direction = "both")
+
+
+require(MASS)
+mtcars.glm <- glm(mpg ~ ., data = mtcars)
+stepwise(mtcars.glm, trace = FALSE)
+stepwise(mtcars.glm, direction="forward/backward")
+
+step <- stepAIC(mtcars.glm, direction="both")
+step$anova # display results
+
+library(relaimpo)
+pack
+calc.relimp(fit,type=c("lmg","last","first","pratt"),
+            rela=TRUE)
+
+library(relaimpo)
+install.packages("relaimpo")
+data(swiss)
+calc.relimp(swiss, 
+            type = c("lmg", "last", "first", "betasq", "pratt", "genizi", "car") )
+# calculation of all available relative importance metrics 
+# non-US version offers the additional metric "pmvd", 
+# i.e. call would be 
+# calc.relimp(cov(swiss), 
+# type = c("lmg", "pmvd", "last", "first", "betasq, "pratt"), 
+# rela = TRUE )
+## same analysis with formula or lm method and a few modified options
+crf <- calc.relimp(Fertility~Agriculture+Examination+Education+Catholic+Infant.Mortality,swiss, 
+                   subset = Catholic>40,
+                   type = c("lmg", "last", "first", "betasq", "pratt"), rela = TRUE )
+crf
+
+
+
+library(relaimpo)
+install.packages("relaimpo")
+
+
+initial.model <- lm(mpg ~., data= mtcars)
+best.model <- step(initial.model, direction = "both")
+summary(best.model)
+?calc.relimp()
+calc.relimp(best.model)
